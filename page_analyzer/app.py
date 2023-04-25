73b3b2c7f64b8026df7a8b651a4e875998011a2a
KarinA <abdullaeva.karina.r@gmail.com>
2023-04-25 13:11:12 +0300

    main: Add possibility for adding urls to database:
    1. Add new dependencies for working with DB and urls;
    2. Add file database.sql with commands to create DB for project;
    3. Edit app.py for recieving data, validate it and insert to DB.

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,53 @@
-from flask import Flask, render_template
+import os
+from datetime import datetime
+from urllib.parse import urlparse
 
+import psycopg2
+import validators
+from dotenv import load_dotenv
+from flask import Flask, flash, redirect, render_template, request, url_for
+
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+SECRET = os.getenv('SECRET_KEY')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/urls', methods=['POST'])
+def urls_post():
+    url_from_request = request.form.to_dict().get('url')
+    parsed_url = urlparse(url_from_request)
+    new_url = f'{parsed_url.scheme}://{parsed_url.netloc}'
+
+    if not validate(new_url):
+        flash('Некорректный URL', 'error')
+        return redirect(url_for('index'))
+
+    created_at = datetime.now()
+
+    connection = psycopg2.connect(DATABASE_URL)
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                try:
+                    cursor.execute("INSERT INTO urls (name, created_at)\
+                                VALUES (%s, %s)", (new_url, created_at))
+                    flash('Страница добавлена!', 'success')
+                except psycopg2.errors.UniqueViolation:
+                    flash('Страница уже существует',
+                          'warning')
+    finally:
+        connection.close()
+
+    return redirect(url_for('index'))
+
+
+def validate(url):
+    return validators.url(url, public=True) and len(url) <= 255
