b96f764ad3a8789ad3df60966ea653e2d109f3a9
KarinA <abdullaeva.karina.r@gmail.com>
2023-05-03 20:36:41 +0300

    main: Update a few moments:
    1. Allocate to DB into special function connect();
    2. Use NamedTuple from psycopg2 for more readable code.

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,13 +2,14 @@ import os
 from datetime import datetime
 
 import psycopg2
+from psycopg2.extras import NamedTupleCursor
 import requests
 from dotenv import load_dotenv
 from flask import Flask, flash, redirect, render_template, request, url_for
 
-from .finder import find_checks, find_url
-from .url import normalize_url, validate_url
+from .finder import connect, find_checks, find_url
 from .parser import parser
+from .url import normalize_url, validate_url
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -26,71 +27,64 @@ def index() -> str:
 @app.route('/urls', methods=['POST'])
 def urls_post() -> str:
     url_from_request = request.form.to_dict().get('url')
-    new_url = normalize_url(url_from_request)
 
-    if not validate_url(new_url):
+    if not validate_url(url_from_request):
         flash('Некорректный URL', 'alert-danger')
         return render_template('index.html'), 422
 
+    new_url = normalize_url(url_from_request)
     now = datetime.now()
     created_at = str(now)[:19]  # cutting off microseconds
 
-    connection = psycopg2.connect(DATABASE_URL)
-    try:
-        with connection:
-            with connection.cursor() as cursor:
-                try:
-                    cursor.execute("INSERT INTO urls (name, created_at)\
-                                   VALUES (%s, %s) RETURNING id",
-                                   (new_url, created_at))
-                    url_id = cursor.fetchone()[0]  # accessing id from tuple
-                    flash('Страница успешно добавлена', 'alert-success')
+    with connect() as connection:
+        with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            try:
+                cursor.execute("INSERT INTO urls (name, created_at)\
+                                VALUES (%s, %s) RETURNING id",
+                               (new_url, created_at))
+                url_info = cursor.fetchone()
+                url_id = url_info.id  # accessing id from named tuple
+                flash('Страница успешно добавлена', 'alert-success')
 
-                except psycopg2.errors.UniqueViolation:
-                    url_id, _, _ = find_url(name=new_url)
-                    flash('Страница уже существует', 'alert-warning')
-    finally:
-        connection.close()
+            except psycopg2.errors.UniqueViolation:
+                url = find_url(name=new_url)
+                url_id = url.id
+                flash('Страница уже существует', 'alert-warning')
 
     return redirect(url_for('one_url', id=url_id))
 
 
 @app.route('/urls', methods=['GET'])
 def urls() -> str:
-    list_of_urls = []
-    connection = psycopg2.connect(DATABASE_URL)
-    try:
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT * FROM urls ORDER BY id DESC")
-                list_of_urls.extend(cursor.fetchall())
-    finally:
-        connection.close()
+    urls = []
 
-    for i, url in enumerate(list_of_urls):
+    with connect() as connection:
+        with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute("SELECT * FROM urls ORDER BY id DESC")
+            urls.extend(cursor.fetchall())
+
+    for i, url in enumerate(urls):
         # accessing info about url's checks by url_id
-        checks = find_checks(url[0])
+        checks = find_checks(url.id)
         if checks:
-            # adding info about url's last check: time & status code
-            list_of_urls[i] = url + (checks[0][6], checks[0][2])
+            # adding info about url's last check
+            urls[i] = url + (checks[0].created_at, checks[0].status_code)
         else:
             continue
 
-    return render_template('list_of_urls.html', urls=list_of_urls)
+    return render_template('list_of_urls.html', urls=urls)
 
 
 @app.route('/urls/<int:id>', methods=['GET'])
 def one_url(id: int) -> str:
-    url_info = find_url(id=id)
+    url = find_url(id=id)
 
-    if url_info is None:
+    if url is None:
         flash('Такой страницы не существует', 'alert-warning')
         return redirect(url_for('index'))
 
-    _, url_name, url_creating_time = url_info
-
-    return render_template('show.html', ID=id, name=url_name,
-                           created_at=url_creating_time,
+    return render_template('show.html', ID=id, name=url.name,
+                           created_at=url.created_at,
                            checks=find_checks(id))
 
 
@@ -99,33 +93,30 @@ def check_url(id: int) -> str:
     now = datetime.now()
     checked_at = str(now)[:19]  # cutting off microseconds
 
-    _, url_name, url_creating_time = find_url(id=id)
+    url = find_url(id=id)
 
     try:
-        with requests.get(url_name) as response:
+        with requests.get(url.name) as response:
             status_code = response.status_code
             if status_code != 200:
                 response.raise_for_status()
 
     except requests.exceptions.RequestException:
         flash('Произошла ошибка при проверке', 'alert-danger')
-        return render_template('show.html', ID=id, name=url_name,
-                               created_at=url_creating_time,
+        return render_template('show.html', ID=id, name=url.name,
+                               created_at=url.created_at,
                                checks=find_checks(id)), 422
 
     h1, title, description = parser(response)
-    connection = psycopg2.connect(DATABASE_URL)
-    try:
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute("INSERT INTO url_checks (url_id, status_code,\
-                               h1, title, description, created_at)\
-                               VALUES (%s, %s, %s, %s, %s, %s)",
-                               (id, status_code, h1,
-                                title, description, checked_at))
-                flash('Страница успешно проверена', 'alert-success')
-    finally:
-        connection.close()
+
+    with connect() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("INSERT INTO url_checks (url_id, status_code,\
+                           h1, title, description, created_at)\
+                           VALUES (%s, %s, %s, %s, %s, %s)",
+                           (id, status_code, h1,
+                            title, description, checked_at))
+            flash('Страница успешно проверена', 'alert-success')
 
     return redirect(url_for('one_url', id=id))
 
--- page_analyzer/finder.py
+++ page_analyzer/finder.py
@@ -2,27 +2,28 @@ import os
 from datetime import datetime
 
 import psycopg2
+from psycopg2.extras import NamedTupleCursor
 from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
+def connect():
+    return psycopg2.connect(DATABASE_URL)
+
+
 def find_url(id: int = None, name: str = None) -> tuple[int, str, datetime]:
     """Find a record in the database table 'url' by id or url's name."""
-    connection = psycopg2.connect(DATABASE_URL)
-    try:
-        with connection:
-            with connection.cursor() as cursor:
-                if id:
-                    cursor.execute("SELECT * FROM urls WHERE id = %s",
-                                   (id, ))
-                elif name:
-                    cursor.execute("SELECT * FROM urls WHERE name = %s",
-                                   (name, ))
-                url_info = cursor.fetchone()
-    finally:
-        connection.close()
+    with connect() as connection:
+        with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            if id:
+                cursor.execute("SELECT * FROM urls WHERE id = %s",
+                               (id, ))
+            elif name:
+                cursor.execute("SELECT * FROM urls WHERE name = %s",
+                               (name, ))
+            url_info = cursor.fetchone()
 
     return url_info
 
@@ -30,14 +31,12 @@ def find_url(id: int = None, name: str = None) -> tuple[int, str, datetime]:
 def find_checks(url_id: int) -> list[tuple[int, str, datetime]]:
     """Find a record in the database table 'url_checks' by url's id."""
     url_check_info = []
-    connection = psycopg2.connect(DATABASE_URL)
-    try:
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT * FROM url_checks WHERE url_id = %s\
-                               ORDER BY id DESC",
-                               (url_id, ))
-                url_check_info.extend(cursor.fetchall())
-    finally:
-        connection.close()
+
+    with connect() as connection:
+        with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute("SELECT * FROM url_checks WHERE url_id = %s\
+                           ORDER BY id DESC",
+                           (url_id, ))
+            url_check_info.extend(cursor.fetchall())
+
     return url_check_info
